ff97ccea51a14894fc8be13dcea9d32f8c9f70dd
totorowg <wangchingying@gmail.com>
2018-02-09 13:28:51 +0800

    執行按鈕用pending intent

--- app/src/main/java/com/cyw/a2018020902/MyWidgetProvider.java
+++ app/src/main/java/com/cyw/a2018020902/MyWidgetProvider.java
@@ -1,9 +1,13 @@
 package com.cyw.a2018020902;
 
+import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 import android.widget.RemoteViews;
 
 /**
@@ -15,10 +19,20 @@ public class MyWidgetProvider extends AppWidgetProvider {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                 R.layout.mywidget);
+        remoteViews.setTextViewText(R.id.textView,"我改了字");
+        remoteViews.setTextViewText(R.id.button,"按鈕");
+        Intent it = new Intent(context, MainActivity.class);
+
+        //按鈕這麼作,用pending intent
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context, 0, it, 0);
+        remoteViews.setOnClickPendingIntent(R.id.button, pendingIntent);
+        //
+
         ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
 // 取得 Widgets 管理器
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
 // 透過管理器來對指定的Widgets 實例進行單項更新
         manager.updateAppWidget(thisWidget, remoteViews);
-    }
-}
+
+}}
